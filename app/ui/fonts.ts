15e34497cf0aabd4105f48a7b0b03d397c324300
Noah Fu <pingchi996@gmail.com>
2024-05-22 20:58:23 +0800

    First-Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Noto_Sans_TC } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+export const notoSansTC = Noto_Sans_TC({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
